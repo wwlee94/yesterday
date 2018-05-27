f0a5cb931b7d1f8f523fd3265e7a515b7974c2e6
kimYSang <qwdbstkd@naver.com>
2018-05-28 00:43:40 +0900

    0528커밋

---
+++ app/src/main/java/com/example/yesterday/yesterday/ClientLoginInfo.java
@@ -0,0 +1,26 @@
+package com.example.yesterday.yesterday;
+
+public class ClientLoginInfo {
+    String id,pass;
+
+    public ClientLoginInfo(String id, String pass){
+        this.id = id;
+        this.pass = pass;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
--- app/src/main/java/com/example/yesterday/yesterday/Login.java
+++ app/src/main/java/com/example/yesterday/yesterday/Login.java
@@ -2,14 +2,22 @@ package com.example.yesterday.yesterday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
     EditText ed_id, ed_pw;
     Button btn_login;
     String sId, sPw;
+    ClientLoginInfo client;
+//    JSONObject jsonObject;
+//    Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +28,15 @@ public class Login extends AppCompatActivity {
         ed_pw = (EditText) findViewById(R.id.PassText);
         btn_login = (Button) findViewById(R.id.loginBtn);
 
-        sId = ed_id.getText().toString();
-        sPw = ed_pw.getText().toString();
+        sId = ed_id.getText().toString();   // id
+        sPw = ed_pw.getText().toString();   // password
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
+        btn_login.setOnClickListener(new View.OnClickListener() {  // 로그인 버튼 리스너
             @Override
             public void onClick(View v) {
-                ed_id.append("정성연 바보");
-
+                client = new ClientLoginInfo(sId,sPw); // 로그인 클라이언트 객체 생성
+                String json = new Gson().toJson(client);
+                Log.i("info","login");
             }
         });
     }
