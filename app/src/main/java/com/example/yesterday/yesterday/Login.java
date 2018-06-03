e937898da1cf8d811728c7a596b1b9215f14a073
kimYSang <qwdbstkd@naver.com>
2018-06-03 21:29:49 +0900

    kakao 연동 시작

--- app/src/main/java/com/example/yesterday/yesterday/Login.java
+++ app/src/main/java/com/example/yesterday/yesterday/Login.java
@@ -67,7 +67,7 @@ public class Login extends AppCompatActivity {
         kakaoLogoutBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                
+
             }
         });
 
