f83773da000db13e1a9f1625e61d0291dcf6b928
wwlee94 <38131683+wwlee94@users.noreply.github.com>
2018-07-05 01:11:17 +0900

    ## Toolbar,statusbar 모두 투명 적용 - HomeFragment에서 다른 fragment로 넘어갈 때 setAutoViewPager 일시 정지 후   다시 HomeFragment로 돌아가면 setAutoViewPager 스레드 새로 생성   (없앴다가 새로 생성하는 것이 더 메모리 효율적 인지는 판단 X) - 추후 고려해야 할 사항  1. 배경화면 변경가능  2. 달력,먹은 메뉴 클릭시 fragment가 아닌 Activity로 구현 할 것     추가화면(fragment)과 수정화면(frag? or activity?) 논의

--- app/src/main/java/com/example/yesterday/yesterday/UI/HomeFragment.java
+++ app/src/main/java/com/example/yesterday/yesterday/UI/HomeFragment.java
@@ -1,82 +1,82 @@
 package com.example.yesterday.yesterday.UI;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.yesterday.yesterday.R;
 import com.example.yesterday.yesterday.UI.ViewPager.Chart1Fragment;
 import com.example.yesterday.yesterday.UI.ViewPager.Chart2Fragment;
 import com.example.yesterday.yesterday.UI.ViewPager.Chart3Fragment;
-
-import java.util.Timer;
-import java.util.TimerTask;
+import com.matthewtamlin.sliding_intro_screen_library.background.BackgroundManager;
+import com.matthewtamlin.sliding_intro_screen_library.background.ColorBlender;
 
 import static android.content.ContentValues.TAG;
 
 //home 화면 Fragment
 public class HomeFragment extends Fragment {
 
-    private Intent intent;
+    private static final int[] BACKGROUND_COLORS = {0xff304FFE, 0xffcc0066, 0xff9900ff};
+    private BackgroundManager backgroundManager = null;
 
+    private Intent intent;
     private ViewGroup rootView;
 
-    private ViewPager viewPager;
+    private ViewPager viewPager = null;
+
     private Button btn_go;
-    private TextView view;
 
     private Fragment[] arrFragment;
 
     private Handler handler;
-    private autoChangeViewPager runnable;
+    private setAutoChangeViewPager setAutoChangeViewPager;
     private Thread thread;
     private boolean isrun;
 
-    private int posit=0;        //페이지 번호
-    int tnum=100;
-
     public HomeFragment() {
         // Required empty public constructor
+
+        //chartFragment
+        arrFragment = new Fragment[3];
+        arrFragment[0] = new Chart1Fragment();
+        arrFragment[1] = new Chart2Fragment();
+        arrFragment[2] = new Chart3Fragment();
+
+        //핸들러 생성 -> UI 변경하려면 무조건 핸들러
         handler = new Handler();
-        runnable = new autoChangeViewPager();
+        //핸들러의 runnable (스레드) 작동 (setAutoChangeViewPager)
+        setAutoChangeViewPager = new setAutoChangeViewPager();
         isrun=true;
-
-        //pause , continue 언제?
+        //배경화면 colormanager
+        configureBackground();
     }
-
     //생성자와 onCreateView만 있어도 ok
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         //인플레이트(inflate) 한다는 것은 동작 가능한 view의 객체로 생성한다는 의미
         //rootView가 플래그먼트 화면으로 보이게 된다.
-
         thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
+                    Log.d(TAG, "Thread 시작");
                     while(isrun) {
                         Thread.sleep(5000);
-                        Log.d(TAG, ""+ tnum--);
                         //UI 스레드
-                        handler.post(runnable);
+                        handler.post(setAutoChangeViewPager);
                     }
-                        Log.d(TAG, "마지막");
-                        isrun=true;
+                    Log.d(TAG, "Thread 종료");
+                    isrun=true;
                 }catch(Exception e) {
                     e.printStackTrace();
                 }
@@ -85,12 +85,8 @@ public class HomeFragment extends Fragment {
         thread.start();
 
         Log.d(TAG, "onCreateView: Thread onCreateView");
-        rootView = (ViewGroup)inflater.inflate(R.layout.fragment_home,container,false);
 
-        arrFragment = new Fragment[3];
-        arrFragment[0] = new Chart1Fragment();
-        arrFragment[1] = new Chart2Fragment();
-        arrFragment[2] = new Chart3Fragment();
+        rootView = (ViewGroup)inflater.inflate(R.layout.fragment_home,container,false);
 
         viewPager=(ViewPager)rootView.findViewById(R.id.viewpager);
         viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
@@ -103,7 +99,28 @@ public class HomeFragment extends Fragment {
                 return arrFragment.length;
             }
         });
-
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (backgroundManager != null) {
+                    backgroundManager.updateBackground(viewPager, position, positionOffset);
+                }
+            }
+            @Override
+            public void onPageSelected(int position) { }
+            @Override
+            public void onPageScrollStateChanged(int state) { }
+        });
+        //| 좌 2 | 중앙 | 우 2 |  이미지를 미리 로딩시키는 메소드
+        viewPager.setOffscreenPageLimit(2);
+        //ViewPager의 mScroller를 이용, 권한 허용하여 원하는 myPager에 duration 설정
+        /*
+        try {
+            mScroller = ViewPager.class.getDeclaredField("mScroller");
+            mScroller.setAccessible(true);
+            mScroller.set(viewPager, new setScrollerDuration(viewPager.getContext(),sInterpolator,1000));
+        }catch(Exception e){ e.printStackTrace(); }
+        */
         btn_go = (Button)rootView.findViewById(R.id.button);
         btn_go.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -116,15 +133,23 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return rootView;
     }
-    public class autoChangeViewPager implements Runnable{
-            public void run() {
-                if (posit == 3) {
-                    posit = 0;
-                }
-                viewPager.setCurrentItem(posit,true);
+    //자동 스크롤 해주는 스레드 - 현재 위치 기준
+    // UI 변경되는 스레드임!!
+    public class setAutoChangeViewPager implements Runnable{
+
+        int posit;        //페이지 번호
+
+        public void run() {
+            posit = viewPager.getCurrentItem();
+            if (posit == 2) {
+                posit = 0;
+                viewPager.setCurrentItem(posit, true);
+            } else {
                 posit++;
-                Log.d(TAG, "****Thread 실행 중****");
+                viewPager.setCurrentItem(posit, true);
             }
+            Log.d(TAG, "****Thread 실행 중****");
+        }
     }
 
     @Override
@@ -140,5 +165,22 @@ public class HomeFragment extends Fragment {
         Log.d(TAG, "onPause: Thread 일시 중지");
     }
 
-
+    //전역 변수로 선언한 backgroundManager = configureBackground로 설정한 backgroundManager
+    private void configureBackground() {
+        this.backgroundManager= new ColorBlender(BACKGROUND_COLORS);
+    }
+    /*
+    public class setScrollerDuration extends Scroller {
+        private int mDuration;
+        public setScrollerDuration(Context context, Interpolator interpolator, int duration) {
+            super(context, interpolator);
+            mDuration = duration;
+        }
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+            // Ignore received duration, use fixed one instead
+            super.startScroll(startX, startY, dx, dy, mDuration);
+        }
+    }
+    */
 }
