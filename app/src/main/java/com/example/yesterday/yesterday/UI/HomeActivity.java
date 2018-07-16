46501cb129bd2bb87516713240d36e9f0e62a1b6
kimYSang <qwdbstkd@naver.com>
2018-07-16 20:30:30 +0900

    -auto login & logout

--- app/src/main/java/com/example/yesterday/yesterday/UI/HomeActivity.java
+++ app/src/main/java/com/example/yesterday/yesterday/UI/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.yesterday.yesterday.UI;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
@@ -12,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.example.yesterday.yesterday.ClientLoginInfo;
 import com.example.yesterday.yesterday.R;
 
 import com.example.yesterday.yesterday.UI.HomeFrags.AddFragment;
@@ -38,13 +41,16 @@ public class HomeActivity extends AppCompatActivity {
     private Toolbar toolbar;
     //FragmentManager
     private FragmentManager fragmentManager;
+    //SharedPreferences
+    SharedPreferences loginSetting;
+    SharedPreferences.Editor editor;
 
     //MaterialDrawer
     private PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("홈").withIcon(R.drawable.ic_home_solid_white).withIconTintingEnabled(true);
     private PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(2).withName("홈_첫번째").withIcon(R.drawable.ic_wb_sunny_black_24dp).withIconTintingEnabled(true);
     private PrimaryDrawerItem item3 = new PrimaryDrawerItem().withIdentifier(3).withName("홈_두번째").withIcon(R.drawable.ic_help_outline_black_24dp).withIconTintingEnabled(true);
     private PrimaryDrawerItem item4 = new PrimaryDrawerItem().withIdentifier(4).withName("섹션_첫번째").withIcon(R.drawable.ic_settings_black_24dp).withIconTintingEnabled(true);
-    private PrimaryDrawerItem item5 = new PrimaryDrawerItem().withIdentifier(5).withName("우엉우엉이짱").withIcon(R.drawable.ic_playlist_add_black_24dp).withIconTintingEnabled(true);
+    private PrimaryDrawerItem logout = new PrimaryDrawerItem().withIdentifier(5).withName("로그아웃").withIcon(R.drawable.ic_playlist_add_black_24dp).withIconTintingEnabled(true);
 
     private SecondaryDrawerItem sectionHeader = new SecondaryDrawerItem().withName("section_header");
 
@@ -64,7 +70,8 @@ public class HomeActivity extends AppCompatActivity {
     ImageView calendarView;
     //
     Intent intent;
-
+    //client
+    ClientLoginInfo client;
 
     public HomeActivity(){
 
@@ -116,16 +123,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //SharedPreference
+        loginSetting = getSharedPreferences("loginSetting",0);
+        editor = loginSetting.edit();
 
         Log.d("TAG","onCreate / 앱 생성(초기화)");
 
         //MaterialDrawer 쓰기위해 toolbar의 id를 가져와 객체 생성
         toolbar=(Toolbar)findViewById(R.id.toolbar);
 
-        //Intent로 로그인 이름 가져옴 -> name은 아직 안쓰임
+        //Intent로 client가져오기  -> client 아직 안쓰임
         intent = getIntent();
-        name = intent.getStringExtra("name");
+        client = (ClientLoginInfo) intent.getSerializableExtra("client");
 
+        //logout listener
+        //item5.set
         //Calendar
         //Calendar View로 넘어가면 밑에 바텀바 focus 어케 해결!?
         calendarView=(ImageView)findViewById(R.id.toolbar_calendar_button);
@@ -137,6 +149,7 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+
         // MaterialDrawer
         // Create the AccountHeader -> 사용자 계정(이미지,이름,이메일)헤더 생성
         headerResult = new AccountHeaderBuilder()
@@ -146,9 +159,10 @@ public class HomeActivity extends AppCompatActivity {
                 .withProfileImagesClickable(false)              //프로필이미지선택X
                 //.withSavedInstance(savedInstanceState)
                 .addProfiles(
-                        new ProfileDrawerItem().withName("woowonLee").withEmail("wwlee94@gmail.com").withIcon(getResources().getDrawable(R.drawable.profile))
+                        new ProfileDrawerItem().withName(client.getName()).withEmail("wwlee94@gmail.com").withIcon(getResources().getDrawable(R.drawable.profile))
                 )
                 .build();
+
         //create the drawer and remember the `Drawer` result object
         result = new DrawerBuilder()
                 .withActivity(this)
@@ -158,13 +172,21 @@ public class HomeActivity extends AppCompatActivity {
                         item1, item2, item3,
                         new DividerDrawerItem(),
                         sectionHeader,
-                        item4,item5
+                        item4,logout
                 )
                 //drawer를 클릭 했을 때 이벤트 처리
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
                         // do something with the clicked item :D
+
+                        //logout clicked
+                        if(drawerItem == logout){
+                            editor.clear();
+                            editor.commit();
+                            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                            startActivity(intent);
+                        }
                         return true;
                     }
                 })
@@ -175,6 +197,8 @@ public class HomeActivity extends AppCompatActivity {
                 .withDrawerLayout(R.layout.material_drawer_fits_not)
                 //.withSavedInstance(savedInstanceState)
                 .build();
+
+
 
         //bottomBar를 tab했을 때 id를 구분해 해당 내부코드를 실행하여 Fragment의 전환이 이루어짐
         bottomBar=(BottomBar)findViewById(R.id.bottomBar);
@@ -202,6 +226,7 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
+
     public void replaceFragment(Fragment fragment){
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.contentContainer, fragment);
--- app/src/main/java/com/example/yesterday/yesterday/UI/LoginActivity.java
+++ app/src/main/java/com/example/yesterday/yesterday/UI/LoginActivity.java
@@ -29,7 +29,7 @@ import com.kakao.util.exception.KakaoException;
 
 public class LoginActivity extends AppCompatActivity {
     EditText id_text, pw_text;
-    Button login_btn,kakaoLogout_btn,join_btn;
+    Button login_btn, kakaoLogout_btn, join_btn;
     CheckBox auto_check;
     TextView jsonText;
     String sId, sPw;
@@ -41,43 +41,48 @@ public class LoginActivity extends AppCompatActivity {
     SharedPreferences.Editor editor;
 
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_login);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
-            id_text = (EditText) findViewById(R.id.id_text);
-            pw_text = (EditText) findViewById(R.id.pw_text);
-            login_btn = (Button) findViewById(R.id.login_btn);
-            kakaoLogout_btn = (Button) findViewById(R.id.kakaoLogout_btn);
-            join_btn = (Button) findViewById(R.id.join_btn);
-            auto_check = (CheckBox) findViewById(R.id.auto_check);
+        id_text = (EditText) findViewById(R.id.id_text);
+        pw_text = (EditText) findViewById(R.id.pw_text);
+        login_btn = (Button) findViewById(R.id.login_btn);
+        kakaoLogout_btn = (Button) findViewById(R.id.kakaoLogout_btn);
+        join_btn = (Button) findViewById(R.id.join_btn);
+        auto_check = (CheckBox) findViewById(R.id.auto_check);
 
-            client = new ClientLoginInfo();
+        client = new ClientLoginInfo();
 
-            loginSetting = getSharedPreferences("loginSetting",0);
-            editor = loginSetting.edit();
+        //login SharedPreferences
+        loginSetting = getSharedPreferences("loginSetting", 0);
+        editor = loginSetting.edit();
 
-            if(loginSetting.getBoolean("Auto_Login_enabled",false)){
-                id_text.setText(loginSetting.getString("ID",""));
-                pw_text.setText(loginSetting.getString("PW",""));
-                auto_check.setChecked(true);
-            }
+        //auto_check true면 id, pw setting
+        if (loginSetting.getBoolean("Auto_Login_enabled", false)) {
+            //id_text.setText(loginSetting.getString("ID",""));
+            //pw_text.setText(loginSetting.getString("PW",""));
+            auto_check.setChecked(true);
+            sId = loginSetting.getString("ID", "");
+            sPw = loginSetting.getString("PW", "");
+            Login(sId, sPw);
+        }
 
-            auto_check.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if(isChecked){
-                        editor.putString("ID",id_text.getText().toString());
-                        editor.putString("PW",pw_text.getText().toString());
-                        editor.putBoolean("Auto_Login_enabled",true);
-                        editor.commit();
-                    }
-                    else{
-                        editor.clear();
-                        editor.commit();
-                    }
+        //auto_check click listener
+        auto_check.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    editor.putString("ID", id_text.getText().toString());
+                    editor.putString("PW", pw_text.getText().toString());
+                    editor.putBoolean("Auto_Login_enabled", true);
+                    editor.commit();
+                } else {
+                    editor.clear();
+                    editor.commit();
                 }
-            });
+            }
+        });
 
         login_btn.setOnClickListener(new View.OnClickListener() {  // 로그인 버튼 리스너
             @Override
@@ -85,25 +90,25 @@ public class LoginActivity extends AppCompatActivity {
                 // 사용자가 입력한 id와 pw값을 받아옴 ..... 리스너 안에서 가져와야함 ㅠ
                 sId = id_text.getText().toString();   // id
                 sPw = pw_text.getText().toString();   // password
-
-
-                // AsyncTask 객체 생성, 호출
-                try {
-                    result  = new LoginServer(sId,sPw).execute().get();
-                } catch (Exception e){
-                    e.getMessage();
-                }
-
-                if(result.equals("fail")){
-                    Toast.makeText(getApplicationContext(), "로그인 실패 입니다.", Toast.LENGTH_LONG).show();
-                }
-                else{
-                    client.setType("회원");
-                    client.setName(result);
-                    intent = new Intent(getApplicationContext(), HomeActivity.class);
-                    intent.putExtra("client",client);
-                    startActivity(intent);
-                }
+                Login(sId, sPw);
+//                // AsyncTask 객체 생성, 호출
+//                try {
+//                    result = new LoginServer(sId, sPw).execute().get();
+//                } catch (Exception e) {
+//                    e.getMessage();
+//                }
+//
+//                Log.d("result", result);
+//
+//                if (result.equals("fail")) {
+//                    Toast.makeText(getApplicationContext(), "로그인 실패 입니다.", Toast.LENGTH_LONG).show();
+//                } else {
+//                    client.setType("회원");
+//                    client.setName(result);
+//                    intent = new Intent(getApplicationContext(), HomeActivity.class);
+//                    intent.putExtra("client", client);
+//                    startActivity(intent);
+//                }
             }
         });
 
@@ -116,7 +121,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         //카카오톡 로그아웃 나중에 메인화면 안에서 다시 구성
-        kakaoLogout_btn.setOnClickListener(new View.OnClickListener(){
+        kakaoLogout_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onClickLogout();
@@ -129,7 +134,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     //카카오톡 로그아웃
-    private void onClickLogout(){
+    private void onClickLogout() {
         UserManagement.requestLogout(new LogoutResponseCallback() {
             @Override
             public void onCompleteLogout() {
@@ -147,6 +152,7 @@ public class LoginActivity extends AppCompatActivity {
                 @Override
                 public void onSessionClosed(ErrorResult errorResult) {
                 }
+
                 @Override
                 public void onNotSignedUp() {
                 }
@@ -157,14 +163,14 @@ public class LoginActivity extends AppCompatActivity {
                     //사용자 ID는 보안상의 문제로 제공하지 않고 일련번호는 제공합니다.
                     long number = userProfile.getId();
 
-                    Log.i("userProfile",Long.toString(number));
+                    Log.i("userProfile", Long.toString(number));
                 }
             });
         }
 
         @Override
         public void onSessionOpenFailed(KakaoException exception) {
-            Log.i("SessionError","kakaoSession failed");
+            Log.i("SessionError", "kakaoSession failed");
         }
     }
 
@@ -175,11 +181,13 @@ public class LoginActivity extends AppCompatActivity {
             public void onSessionClosed(ErrorResult errorResult) {
                 Log.d("onSessionClosed", "onSessionClosed1 =" + errorResult);
             }
+
             @Override
             public void onNotSignedUp() {
                 //카카오톡 회원이 아닐시
                 Log.d("NotSignedUp", "onNotSignedUp ");
             }
+
             @Override
             public void onSuccess(UserProfile result) { //로그인 성공, 원하는 정보 가져오기
                 Log.e("UserProfile", result.toString());
@@ -189,10 +197,31 @@ public class LoginActivity extends AppCompatActivity {
                 client.setType("카카오");
                 //homeActivity로 전달
                 intent = new Intent(getApplicationContext(), HomeActivity.class);
-                intent.putExtra("client",client);
+                intent.putExtra("client", client);
                 //client.setBirth(result.get);
                 startActivity(intent);
             }
         });
+    }
+
+    public void Login(String id, String pw) {
+        // AsyncTask 객체 생성, 호출
+        try {
+            result = new LoginServer(id, pw).execute().get();
+        } catch (Exception e) {
+            e.getMessage();
+        }
+
+        Log.d("result", result);
+
+        if (result.equals("fail")) {
+            Toast.makeText(getApplicationContext(), "로그인 실패 입니다.", Toast.LENGTH_LONG).show();
+        } else {
+            client.setType("회원");
+            client.setName(result);
+            intent = new Intent(getApplicationContext(), HomeActivity.class);
+            intent.putExtra("client", client);
+            startActivity(intent);
+        }
     }
 }
