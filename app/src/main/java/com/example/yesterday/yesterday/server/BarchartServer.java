b96cf0fc55b4c4a629f9b3eb4fbb0220e7b38e0f
JeongSYeon <tjdds1109@naver.com>
2018-08-21 21:48:05 +0900

    바차트, 푸드 리스트 서버 추가

--- app/src/main/java/com/example/yesterday/yesterday/server/BarchartServer.java
+++ app/src/main/java/com/example/yesterday/yesterday/server/BarchartServer.java
@@ -2,6 +2,8 @@ package com.example.yesterday.yesterday.server;
 
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
+
 import java.io.IOException;
 
 import okhttp3.FormBody;
@@ -11,16 +13,19 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 //로그인 서버와 연결하는 클래스
-public class LoginServer extends AsyncTask<Void,Void,String> {
+public class BarchartServer extends AsyncTask<Void,Void,String> {
     String parent_id;
-    String parent_pw;
+    String startDate;
+    String endDate;
     String answer;
+    JSONArray JArray;
 
     private static final String  WEBIP = "192.168.0.4";
 
-    public LoginServer(String parent_id,String parent_pw) { //로그인 id, pw 받기
+    public BarchartServer(String parent_id,String startDate,String endDate ) { //로그인 id 받기
+        this.startDate = startDate;
+        this.endDate = endDate;
         this.parent_id = parent_id;
-        this.parent_pw = parent_pw;
     }
 
     @Override
@@ -31,17 +36,20 @@ public class LoginServer extends AsyncTask<Void,Void,String> {
         RequestBody requestBody = null;
 
         //보낼 데이터를 파라미터 형식으로 body에 넣음
-        requestBody = new FormBody.Builder().add("parent_id",parent_id).add("parent_pw",parent_pw).build();
+        requestBody = new FormBody.Builder().add("parent_id",parent_id).add("startDate",startDate).add("endDate",endDate).build();
 
         // post형식으로 url로 만든 body를 보냄
         Request request = new Request.Builder()
-                .url("http://"+ WEBIP + ":80/skuniv/login")
+                .url("http://"+ WEBIP + ":80/skuniv/DrawChart")
                 .post(requestBody)
                 .build();
         try {
             response = client.newCall(request).execute();
             /////////////////////////////////// newcall 하고 응답받기를 기다리는중
+
+            //제이슨 값 받기
             answer = response.body().string();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- app/src/main/java/com/example/yesterday/yesterday/server/FoodListServer.java
+++ app/src/main/java/com/example/yesterday/yesterday/server/FoodListServer.java
@@ -2,6 +2,8 @@ package com.example.yesterday.yesterday.server;
 
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
+
 import java.io.IOException;
 
 import okhttp3.FormBody;
@@ -11,16 +13,17 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 //로그인 서버와 연결하는 클래스
-public class LoginServer extends AsyncTask<Void,Void,String> {
+public class FoodListServer extends AsyncTask<Void,Void,String> {
     String parent_id;
-    String parent_pw;
+    String date;
     String answer;
+    JSONArray JArray;
 
     private static final String  WEBIP = "192.168.0.4";
 
-    public LoginServer(String parent_id,String parent_pw) { //로그인 id, pw 받기
+    public FoodListServer(String parent_id, String Date ) { //로그인 id 받기
         this.parent_id = parent_id;
-        this.parent_pw = parent_pw;
+        this.date = Date;
     }
 
     @Override
@@ -31,17 +34,20 @@ public class LoginServer extends AsyncTask<Void,Void,String> {
         RequestBody requestBody = null;
 
         //보낼 데이터를 파라미터 형식으로 body에 넣음
-        requestBody = new FormBody.Builder().add("parent_id",parent_id).add("parent_pw",parent_pw).build();
+        requestBody = new FormBody.Builder().add("parent_id",parent_id).add("date",date).build();
 
         // post형식으로 url로 만든 body를 보냄
         Request request = new Request.Builder()
-                .url("http://"+ WEBIP + ":80/skuniv/login")
+                .url("http://"+ WEBIP + ":80/skuniv/showFoodList")
                 .post(requestBody)
                 .build();
         try {
             response = client.newCall(request).execute();
             /////////////////////////////////// newcall 하고 응답받기를 기다리는중
+
+            //제이슨 값 받기
             answer = response.body().string();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
